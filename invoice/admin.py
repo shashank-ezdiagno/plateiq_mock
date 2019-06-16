81ec51890c708942160c2f8896f8115ac506f60b
shashank <shashank@ezdiagno.com>
2019-06-16 16:23:30 +0530

    adding admin interface

--- invoice/admin.py
+++ invoice/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+
+from .models import Buyer, File, Invoice, InvoiceItem, Vendor, VendorItem
+
+admin.site.register(Buyer)
+admin.site.register(File)
+admin.site.register(Invoice)
+admin.site.register(InvoiceItem)
+admin.site.register(Vendor)
+admin.site.register(VendorItem)
